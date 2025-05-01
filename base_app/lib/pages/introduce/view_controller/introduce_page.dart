c98eec235236140634ec0e5fd75463c29dd8dd28
duynt <thanhduy1812@gmail.com>
2025-05-01 20:40:30 +0700

    add example

---
+++ base_app/lib/pages/introduce/view_controller/introduce_page.dart
@@ -0,0 +1,16 @@
+import 'package:base_app/pages/introduce/view_model/introduce_page_view_model.dart';
+import 'package:vnl_common_ui/vnl_ui.dart';
+import '../../base/view_controller/page_view_controller.dart';
+
+class IntroducePage extends PageViewController<IntroducePageViewModel> {
+  const IntroducePage({super.key, required super.viewModel});
+  @override
+  Widget buildBody(BuildContext pageContext) {
+    return SizedBox();
+  }
+
+  @override
+  IntroducePageState createState() => IntroducePageState();
+}
+
+class IntroducePageState extends PageViewControllerState<IntroducePage> {}
---
+++ base_app/lib/pages/introduce/view_model/introduce_page_view_model.dart
@@ -0,0 +1,7 @@
+import 'package:base_app/pages/base/view_model/page_view_model.dart';
+
+class IntroducePageViewModel extends PageViewModel {
+  IntroducePageViewModel() {
+    title = "Introduce";
+  }
+}
